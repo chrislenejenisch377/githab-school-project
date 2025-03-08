5dd852d73b67eb08afc54cf6e982c93d47668089
chrislenejenisch377 <chrislenejenisch377@gmail.com>
2025-03-08 11:05:02 +0000

    Add 3.cpp

---
+++ src/3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // Ask user for their name
+    cout << "What is your name? ";
+    string name;
+    cin >> name;
+
+    // Greet the user by name
+    cout << "Hello, " << name << "!" << endl;
+
+    // Print a message to the console
+    cout << "Welcome to this amazing school project!" << endl;
+
+    return 0;
+}
